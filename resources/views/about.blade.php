d7d438490b5adc5526668202673953f5764c0a6e
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-28 10:24:44 +0700

    Add html config

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,15 @@
-<?php
-echo "About";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>About</title>
+</head>
+
+<body>
+    <h1>About Page!</h1>
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,15 @@
-<?php
-echo "Home Page";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Laravel</title>
+</head>
+
+<body>
+    <h1>Welcome to Home Page!</h1>
+</body>
+
+</html>
