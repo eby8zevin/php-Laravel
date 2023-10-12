721f1854c7d7c5b139a0a14a1b9daf287906b8c9
Fatkhan Akbar <125345640+fatkhan05@users.noreply.github.com>
2023-10-12 13:15:01 +0700

    Create form message

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="header">
+        <h4>Contact</h4>
+        <h2>Hubungi Saya</h2>
+        <form action="#">
+          <div class="mb-3">
+            <label for="inputNama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="nama">
+          </div>
+          <div class="mb-3">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="mb-3">
+            <label for="inputPesan" class="form-label">Pesan</label>
+            <input type="text" class="form-control" id="pesan">
+          </div>
+          <button type="submit" class="btn btn-primary col-6 rounded-pill">Kirim</button>
+        </form>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
